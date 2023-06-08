6013110d8c43b8e2f1ee9963720301f98c57a9c9
Avery <avery@avery.cafe>
2023-06-08 14:58:10 -0400

    Don't log config on app start

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ fn main() {
     gstgtk4::plugin_register_static().expect("Error registering GST GTK4 plugin");
 
     let config = Config::new().expect("Error creating config.");
-    println!("Config: {:#?}", config);
 
     let app = RelmApp::new("cafe.avery.jellything");
     relm4_icons::initialize_icons();
