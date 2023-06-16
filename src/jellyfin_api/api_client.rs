a03d125c096e6ca6a2b17bdf383c6dc618016959
Avery <avery@avery.cafe>
2023-06-16 14:55:11 -0400

    Fix mitmproxy cert being used in release profile

--- src/jellyfin_api/api_client.rs
+++ src/jellyfin_api/api_client.rs
@@ -4,7 +4,7 @@ use reqwest::{header::HeaderMap, Url};
 
 use crate::config::{Account, Config, Server};
 
-use super::util::{auth_header::get_auth_header, mitmproxy::mitmproxy_cert, url::httpify};
+use super::util::{auth_header::get_auth_header, url::httpify};
 
 pub static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
@@ -28,14 +28,18 @@ impl ApiClient {
         let mut headers = HeaderMap::new();
         headers.insert(reqwest::header::AUTHORIZATION, auth_header.parse().unwrap());
 
+        #[cfg_attr(not(debug_assertions), allow(unused_mut))]
         let mut client = reqwest::Client::builder()
             .user_agent(APP_USER_AGENT)
             .default_headers(headers);
 
         #[cfg(debug_assertions)]
-        let mitmproxy_cert = mitmproxy_cert();
-        if let Some(mitmproxy_cert) = mitmproxy_cert {
-            client = client.add_root_certificate(mitmproxy_cert);
+        {
+            use super::util::mitmproxy::mitmproxy_cert;
+            let mitmproxy_cert = mitmproxy_cert();
+            if let Some(mitmproxy_cert) = mitmproxy_cert {
+                client = client.add_root_certificate(mitmproxy_cert);
+            }
         }
 
         let client = client.build().unwrap();
--- src/jellyfin_api/unauthed_client.rs
+++ src/jellyfin_api/unauthed_client.rs
@@ -1,14 +1,18 @@
 use reqwest::Client;
 
-use super::{api_client::APP_USER_AGENT, util::mitmproxy::mitmproxy_cert};
+use super::api_client::APP_USER_AGENT;
 
 pub fn get_unauthed_client() -> Client {
+    #[cfg_attr(not(debug_assertions), allow(unused_mut))]
     let mut client = Client::builder().user_agent(APP_USER_AGENT);
 
     #[cfg(debug_assertions)]
-    let mitmproxy_cert = mitmproxy_cert();
-    if let Some(mitmproxy_cert) = mitmproxy_cert {
-        client = client.add_root_certificate(mitmproxy_cert);
+    {
+        use super::util::mitmproxy::mitmproxy_cert;
+        let mitmproxy_cert = mitmproxy_cert();
+        if let Some(mitmproxy_cert) = mitmproxy_cert {
+            client = client.add_root_certificate(mitmproxy_cert);
+        }
     }
 
     client.build().unwrap()
