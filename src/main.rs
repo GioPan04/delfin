1c44566bd7c5a4a5d67a41a0cceec0f7eb748c60
Avery <avery@avery.cafe>
2023-09-14 09:50:38 -0400

    Use GTK at-rules in CSS

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,23 @@ fn main() {
 }
 
 fn load_css() {
+    // TODO: move this to a dev dependency once this is done at compile time
+    use regex::Regex;
+
     // TODO: compile sass in build.rs
     let css = grass::from_path("src/app.scss", &grass::Options::default()).unwrap();
+
+    // Output GTK's at-rules. Sass doesn't support custom at-rules, so we wrap
+    // them in a string and unwrap them after Sass has done it's thing.
+
+    // GTK colour expressions - these are similar to Sass's colour functions,
+    // but they can operate on GTK colour variables.
+    let re_gtk_expressions = Regex::new(r#""--gtk-(.+)""#).unwrap();
+    // GTK colour variables
+    let re_gtk_colours = Regex::new(r#""(@.+)""#).unwrap();
+
+    let css = re_gtk_expressions.replace_all(&css, "$1");
+    let css = re_gtk_colours.replace_all(&css, "$1");
+
     relm4::set_global_css(&css);
 }
