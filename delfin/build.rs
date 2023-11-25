1e21f3d1eaa0dda564896a99edd6f994c3ed5cee
Avery <avery@avery.cafe>
2023-11-25 14:35:24 -0500

    Compile styles at compile time

--- delfin/build.rs
+++ delfin/build.rs
@@ -1,4 +1,10 @@
+use std::{env, fs, path::PathBuf};
+
+use regex::Regex;
+
 fn main() {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
     println!(
         "cargo:rerun-if-changed={}",
         std::env::current_dir()
@@ -9,4 +15,24 @@ fn main() {
     );
     println!("cargo:rustc-link-search=/app/lib");
     println!("cargo:rustc-link-search=video_player_mpv/sys");
+
+    build_css(out_dir);
+}
+
+fn build_css(out_dir: PathBuf) {
+    let css = grass::from_string(include_str!("src/app.scss"), &grass::Options::default()).unwrap();
+
+    // Output GTK's at-rules. Sass doesn't support custom at-rules, so we wrap
+    // them in a string and unwrap them after Sass has done it's thing.
+
+    // GTK colour expressions - these are similar to Sass's colour functions,
+    // but they can operate on GTK colour variables.
+    let re_gtk_expressions = Regex::new(r#""--gtk-(.+)""#).unwrap();
+    // GTK colour variables
+    let re_gtk_colours = Regex::new(r#""(@.+)""#).unwrap();
+
+    let css = re_gtk_expressions.replace_all(&css, "$1");
+    let css = re_gtk_colours.replace_all(&css, "$1");
+
+    fs::write(out_dir.join("app.css"), &*css).unwrap();
 }
--- delfin/src/main.rs
+++ delfin/src/main.rs
@@ -19,32 +19,14 @@ fn main() {
     load_resources().expect("Error loading resources");
 
     let app = RelmApp::new(APP_ID);
-
     relm4_icons::initialize_icons();
     load_css();
     app.with_broker(&APP_BROKER).run::<App>(());
 }
 
 fn load_css() {
-    // TODO: move this to a dev dependency once this is done at compile time
-    use regex::Regex;
-
-    // TODO: compile sass in build.rs
-    let css = grass::from_string(include_str!("app.scss"), &grass::Options::default()).unwrap();
-
-    // Output GTK's at-rules. Sass doesn't support custom at-rules, so we wrap
-    // them in a string and unwrap them after Sass has done it's thing.
-
-    // GTK colour expressions - these are similar to Sass's colour functions,
-    // but they can operate on GTK colour variables.
-    let re_gtk_expressions = Regex::new(r#""--gtk-(.+)""#).unwrap();
-    // GTK colour variables
-    let re_gtk_colours = Regex::new(r#""(@.+)""#).unwrap();
-
-    let css = re_gtk_expressions.replace_all(&css, "$1");
-    let css = re_gtk_colours.replace_all(&css, "$1");
-
-    relm4::set_global_css(&css);
+    let css = include_str!(concat!(env!("OUT_DIR"), "/app.css"));
+    relm4::set_global_css(css);
 }
 
 fn load_resources() -> Result<()> {
